a62a63776c7d0997e346f1bb505a678ef773df6d
MELOKAI <134931552+MELOKAI@users.noreply.github.com>
2024-08-09 11:44:51 -0300

    09/08

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express ();
 const porta = 3333;
 app.listen (porta, () => {
     console.log ('Servidor iniciado na porta' + porta);
-    //console.log (`servidor iniciado na porta ${porta}`);
-
+    app.get('/', (request, response) => {
+        response.send ('Hello word');
+    });
 });
