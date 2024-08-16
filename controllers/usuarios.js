ce4ea09f3e3377e125d872bcaff16955b56409cc
MELOKAI <134931552+MELOKAI@users.noreply.github.com>
2024-08-16 11:34:08 -0300

    16/08

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -5,7 +5,52 @@ module.exports = {
         try {            
             return response.status(200).json({
                 sucesso: true, 
-                mensagem: 'Lista de usuários.', 
+                mensagem: 'Lista de usuário.', 
+                dados: null
+            });
+        } catch (error) {
+            return response.status(500).json({
+                sucesso: false,
+                mensagem: 'Erro na requisição.',
+                dados: error.message
+            });
+        }
+    }, 
+    async cadastrarUsuarios(request, response) {
+        try {            
+            return response.status(200).json({
+                sucesso: true, 
+                mensagem: 'cadastro de usuário.', 
+                dados: null
+            });
+        } catch (error) {
+            return response.status(500).json({
+                sucesso: false,
+                mensagem: 'Erro na requisição.',
+                dados: error.message
+            });
+        }
+    }, 
+    async editarUsuarios(request, response) {
+        try {            
+            return response.status(200).json({
+                sucesso: true, 
+                mensagem: 'editar usuário.', 
+                dados: null
+            });
+        } catch (error) {
+            return response.status(500).json({
+                sucesso: false,
+                mensagem: 'Erro na requisição.',
+                dados: error.message
+            });
+        }
+    }, 
+    async apagarUsuarios(request, response) {
+        try {            
+            return response.status(200).json({
+                sucesso: true, 
+                mensagem: 'apagar usuário.', 
                 dados: null
             });
         } catch (error) {
